952054bbbc3e930522ec7c1a8d0803eba86fc01c
wenbinwei <W.Wei@sheffield.ac.uk>
2017-11-17 22:10:29 +0000

    Updated example data to include 500 genes.

--- R/data.R
+++ R/data.R
@@ -1,15 +1,15 @@
 #' exprs.1
 #'
-#' expression of 200 genes in 14 samples (GSM94988 to GSM95001) of yeast after pulses
+#' expression of 500 genes in 14 samples (GSM94988 to GSM95001) of yeast after pulses
 #' 2 g/l glucose, \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE4158}.
 #'
-#' @format A data frame with 200 genes and 14 samples.
+#' @format A data frame with 500 genes and 14 samples.
 "exprs.1"
 
 #' exprs.1
 #'
-#' expression of 200 genes in 14 samples (GSM94988 to GSM95001) of yeast after pulses
+#' expression of 500 genes in 14 samples (GSM94988 to GSM95001) of yeast after pulses
 #' 0.2 g/l glucose, \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE4158}.
 #'
-#' @format A data frame with 200 genes and 12 samples.
+#' @format A data frame with 500 genes and 12 samples.
 "exprs.2"
